7d3199d0b4ea692a4da3c65807cad3966e459dce
Han Verstraete <welteki@pm.me>
2022-01-03 22:45:24 +0100

    Improve gh login workaround

--- overlay/default.nix
+++ overlay/default.nix
@@ -12,15 +12,23 @@ final: prev:
   # Workaround to make gh auth login work untill
   # https://github.com/cli/cli/issues/4955 is fixed
   gh-login = final.writeShellScriptBin "gh-login" ''
-    tmp_config_dir=$(${final.coreutils}/bin/mktemp -d gh-login.XXXX)
+    tmp_config_dir=$(${final.coreutils}/bin/mktemp -p /tmp -d gh-login.XXXX)
     
     # Cleanup working dir on shell exit
     trap '${final.coreutils}/bin/rm -rf -- "$tmp_config_dir"' EXIT
 
-    cp $HOME/.config/gh/hosts.yml $tmp_config_dir/hosts.yml
+    if [ ! -z "$GH_CONFIG_DIR"]; then
+      gh_hosts_config=$GH_CONFIG_DIR/hosts.yml
+    elif [ ! -z "$XDG_CONFIG_HOME" ]; then
+      gh_hosts_config=$XDG_CONFIG_HOME/gh/hosts.yml
+    else
+      gh_hosts_config=$HOME/.config/gh/hosts.yml
+    fi
+
+    [ -f "$gh_hosts_config" ] && cp $gh_hosts_config $tmp_config_dir/hosts.yml
 
     GH_CONFIG_DIR=$tmp_config_dir ${final.gh}/bin/gh auth login $@
 
-    cp $tmp_config_dir/hosts.yml $HOME/.config/gh/hosts.yml
+    [ -f "$tmp_config_dir/hosts.yml" ] && cp $tmp_config_dir/hosts.yml $gh_hosts_config
   '';
 }
