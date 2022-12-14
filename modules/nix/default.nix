88b3e268bb8bf1c3b40478fb6e9e78a5b027001d
Han Verstraete <welteki@pm.me>
2022-12-14 23:49:17 +0100

    Rename nix options

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -15,25 +15,27 @@
         options = "--delete-older-then 30d";
       };
 
-      autoOptimiseStore = true;
-
-      requireSignedBinaryCaches = true;
-      binaryCaches = [
-        "https://welteki.cachix.org"
-      ];
-      binaryCachePublicKeys = [
-        "welteki.cachix.org-1:zb0txiNEbjq9Fx7svp4LhTgFIQHKSa5ESi7QlLFjjQY="
-      ];
-
       extraOptions = lib.mkIf config.welteki.nix.enableFlakes ''
         experimental-features = nix-command flakes
       '';
 
-      # Allow users with sudo to modify the nix store
-      trustedUsers = [
-        "root"
-        "@wheel"
-      ];
+      settings = {
+        require-sigs = true;
+        auto-optimise-store = true;
+
+        substituters = [
+          "https://welteki.cachix.org"
+        ];
+        trusted-public-keys = [
+          "welteki.cachix.org-1:zb0txiNEbjq9Fx7svp4LhTgFIQHKSa5ESi7QlLFjjQY="
+        ];
+
+        # Allow users with sudo to modify the nix store
+        trusted-users = [
+          "root"
+          "@wheel"
+        ];
+      };
     };
   };
 }
