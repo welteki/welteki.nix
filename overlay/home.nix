023200a1dd92f86410c09dddc649350a009ed1ee
Han Verstraete <welteki@pm.me>
2023-09-01 23:50:15 +0200

    Remove lazygit from home overlay

--- overlay/home.nix
+++ overlay/home.nix
@@ -1,11 +1,11 @@
 final: prev:
 {
-  lazygit =
-    final.writeShellScriptBin "lazygit" ''
-      if [ -e ./lg_config.yml ]; then
-        ${prev.lazygit}/bin/lazygit --use-config-file=$HOME/.config/lazygit/config.yml,./lg_config.yml $@
-      else
-        ${prev.lazygit}/bin/lazygit $@
-      fi
-    '';
+  # lazygit =
+  #   final.writeShellScriptBin "lazygit" ''
+  #     if [ -e ./lg_config.yml ]; then
+  #       ${prev.lazygit}/bin/lazygit --use-config-file=$HOME/.config/lazygit/config.yml,./lg_config.yml $@
+  #     else
+  #       ${prev.lazygit}/bin/lazygit $@
+  #     fi
+  #   '';
 }
