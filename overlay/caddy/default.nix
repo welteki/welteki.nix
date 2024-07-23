79fbb82700b7a64b902720b05f2bb1d2e824a03f
Han Verstraete <welteki@pm.me>
2024-07-23 19:17:00 +0200

    caddy: 2.4.4 -> 2.8.4

--- overlay/caddy/default.nix
+++ overlay/caddy/default.nix
@@ -1,9 +1,9 @@
 { buildGoModule, ... }:
 buildGoModule {
   pname = "caddy-custom";
-  version = "2.4.4";
+  version = "2.8.4";
 
   src = ./.;
 
-  vendorSha256 = "1ppx5z4ab7rrilsggs5j3p3nkjflfwliyxjq0ns8zvqlqviz3cvg";
+  vendorHash = "sha256-E38arFv5k8gRh/eiAA2u9EYxWpyMLCIogHIZVJrMuq4=";
 }
