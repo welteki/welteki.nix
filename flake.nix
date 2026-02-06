9c9fab89104ec5ff192647474c3da879920ddb74
Han Verstraete <welteki@pm.me>
2026-02-06 09:47:39 +0100

    Remove home-manager configurations

--- flake.nix
+++ flake.nix
@@ -2,136 +2,59 @@
   description = "Welteki's Nix library";
 
   inputs = {
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs-unstable";
-    };
     vscode-server = {
       url = "github:msteen/nixos-vscode-server";
       flake = false;
     };
     nixpkgs.url = "nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     utils.url = "github:numtide/flake-utils";
     devenv.url = "github:cachix/devenv/v1.8";
     inlets.url = "github:welteki/inlets-nix";
   };
 
   nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     extra-substituters = "https://devenv.cachix.org";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, utils, ... }@inputs: {
-    overlays = {
-      default = import ./overlay/default.nix inputs;
-      home = import ./overlay/home.nix inputs;
-    };
-
-    nixosModules = rec {
-      vscode-server = import inputs.vscode-server;
-      auto-fix-vscode-server = vscode-server;
-      common = import ./modules/common.nix;
-      welteki-users = import ./modules/welteki-users.nix;
-      hetzner-cloud = import ./modules/virtualization/hetzner-cloud.nix;
-      home = import ./modules/home.nix;
-    };
-  } // utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.default ];
-      };
-
-      pkgs-home = import nixpkgs-unstable {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ self.overlays.home ];
-      };
-    in
+  outputs = { self, nixpkgs, utils, ... }@inputs:
     {
-      legacyPackages.homeConfigurations = {
-        welteki = inputs.home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs-home;
-          modules = [
-            self.nixosModules.home
-            ({ config, pkgs, lib, ... }: {
-              # Let Home Manager install and manage itself.
-              programs.home-manager.enable = true;
+      overlays = {
+        default = import ./overlay/default.nix inputs;
+        home = import ./overlay/home.nix inputs;
+      };
 
-              fonts.fontconfig.enable = true;
-
-              home = {
-                username = "welteki";
-                homeDirectory = if pkgs.stdenv.isDarwin then "/Users/welteki" else "/home/welteki";
-                stateVersion = "22.05";
-
-                packages = [
-                  pkgs.nerd-fonts.fira-code
-                ] ++ lib.optionals pkgs.stdenv.isDarwin [
-                  # Ensure at least bash v5 on macOS
-                  pkgs.bash
-                ];
-              };
-            })
-          ];
+      nixosModules = rec {
+        vscode-server = import inputs.vscode-server;
+        auto-fix-vscode-server = vscode-server;
+        common = import ./modules/common.nix;
+        welteki-users = import ./modules/welteki-users.nix;
+        hetzner-cloud = import ./modules/virtualization/hetzner-cloud.nix;
+        home = import ./modules/home.nix;
+      };
+    } // utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
+      in {
+        packages = {
+          inherit (pkgs)
+            inlets actuated-cli caddy devenv mass-deploy kubetrim
+            nats-server-dev;
         };
 
-        welteki-mb-air = inputs.home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgs-home;
+        devShells.default = inputs.devenv.lib.mkShell {
+          inherit inputs pkgs;
           modules = [
-            self.nixosModules.home
             ({ pkgs, ... }: {
-              # Let Home Manager install and manage itself.
-              programs.home-manager.enable = true;
-
-              fonts.fontconfig.enable = true;
-
-              home = {
-                username = "welteki";
-                homeDirectory = "/Users/welteki";
-                stateVersion = "22.05";
-
-                packages = [
-                  pkgs.nerd-fonts.fira-code
-                ];
-              };
-
-              programs = {
-                kitty = {
-                  enable = true;
-                  font = {
-                    name = "FiraCode Nerd Font Mono";
-                    size = 11.0;
-                  };
-                };
-              };
-
+              languages.nix.enable = true;
+              languages.nix.lsp.package = pkgs.nixd;
+              languages.go.enable = true;
             })
           ];
         };
-      };
-
-      packages = {
-        inherit (pkgs)
-          inlets
-          actuated-cli
-          caddy
-          devenv
-          mass-deploy
-          kubetrim
-          nats-server-dev;
-      };
-
-      devShells.default = inputs.devenv.lib.mkShell {
-        inherit inputs pkgs;
-        modules = [
-          ({ pkgs, ... }: {
-            languages.nix.enable = true;
-            languages.nix.lsp.package = pkgs.nixd;
-            languages.go.enable = true;
-          })
-        ];
-      };
-    });
+      });
 }
--- modules/home.nix
+++ modules/home.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 let
   aliases = {
     g = "git";
     lg = "lazygit";
-    ofpasswd = "kubectl get secret -n openfaas basic-auth -o jsonpath='{.data.basic-auth-password}' | base64 --decode; echo";
-    oflogin = "kubectl get secret -n openfaas basic-auth -o jsonpath='{.data.basic-auth-password}' | base64 --decode | faas-cli login --username admin --password-stdin";
+    ofpasswd =
+      "kubectl get secret -n openfaas basic-auth -o jsonpath='{.data.basic-auth-password}' | base64 --decode; echo";
+    oflogin =
+      "kubectl get secret -n openfaas basic-auth -o jsonpath='{.data.basic-auth-password}' | base64 --decode | faas-cli login --username admin --password-stdin";
     kctl = "kubectl";
     kctx = "kubectx";
     kns = "kubens";
   };
-in
-{
+in {
   programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
     git = {
       enable = true;
       userName = "Han Verstraete";
@@ -28,7 +32,7 @@ in
         mff = "merge --ff-only";
         l = "log";
         lo = "log --oneline";
-        signoff = ''!f() { git rebase --signoff @~''${1:-1}; }; f'';
+        signoff = "!f() { git rebase --signoff @~\${1:-1}; }; f";
       };
       ignores = [
         # macOS.gitignore source:https://github.com/github/gitignore/blob/master/Global/macOS.gitignore
@@ -43,19 +47,15 @@ in
         # Lazygit config
         "lg_config.yml"
       ];
-      extraConfig = {
-        init.defaultBranch = "main";
-      };
-      includes = [
-        {
-          path = pkgs.writeText "git-openfaas" ''
-            [user]
-                name = Han Verstraete (OpenFaaS Ltd)
-                email = han@openfaas.com
-          '';
-          condition = "gitdir:~/code/openfaas/";
-        }
-      ];
+      extraConfig = { init.defaultBranch = "main"; };
+      includes = [{
+        path = pkgs.writeText "git-openfaas" ''
+          [user]
+              name = Han Verstraete (OpenFaaS Ltd)
+              email = han@openfaas.com
+        '';
+        condition = "gitdir:~/code/openfaas/";
+      }];
     };
 
     gh = {
@@ -63,27 +63,23 @@ in
       gitCredentialHelper.enable = true;
     };
 
-    tmux = {
-      enable = true;
-    };
+    tmux = { enable = true; };
 
     zsh = {
       enable = true;
       enableCompletion = true;
       autosuggestion.enable = true;
       shellAliases = aliases;
-      plugins = [
-        {
-          name = "zsh-syntax-highlighting";
-          file = "zsh-syntax-highlighting.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "zsh-users";
-            repo = "zsh-syntax-highlighting";
-            rev = "0.8.0";
-            sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
-          };
-        }
-      ];
+      plugins = [{
+        name = "zsh-syntax-highlighting";
+        file = "zsh-syntax-highlighting.plugin.zsh";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-syntax-highlighting";
+          rev = "0.8.0";
+          sha256 = "sha256-iJdWopZwHpSyYl5/FQXEW7gl/SrKaYDEtTH9cGP7iPo=";
+        };
+      }];
     };
 
     bash = {
@@ -101,50 +97,26 @@ in
       enableZshIntegration = true;
       enableBashIntegration = true;
       settings = {
-        docker_context = {
-          symbol = " ";
-        };
-        git_branch = {
-          symbol = " ";
-        };
-        golang = {
-          symbol = " ";
-        };
-        java = {
-          symbol = " ";
-        };
+        docker_context = { symbol = " "; };
+        git_branch = { symbol = " "; };
+        golang = { symbol = " "; };
+        java = { symbol = " "; };
         kubernetes = {
           disabled = false;
           symbol = "󱃾 ";
         };
-        nix_shell = {
-          symbol = " ";
-        };
-        nodejs = {
-          symbol = " ";
-        };
-        package = {
-          symbol = " ";
-        };
-        python = {
-          symbol = " ";
-        };
-        terraform = {
-          symbol = "  ";
-        };
-        direnv = {
-          disabled = false;
-        };
+        nix_shell = { symbol = " "; };
+        nodejs = { symbol = " "; };
+        package = { symbol = " "; };
+        python = { symbol = " "; };
+        terraform = { symbol = "  "; };
+        direnv = { disabled = false; };
         shlvl = {
           disabled = false;
           symbol = "󰹹 ";
         };
-        git_status = {
-          disabled = true;
-        };
-        hostname = {
-          ssh_symbol = " ";
-        };
+        git_status = { disabled = true; };
+        hostname = { ssh_symbol = " "; };
       };
     };
 
@@ -159,36 +131,39 @@ in
     };
     lazygit.enable = true;
     atuin = {
-      enable = false;
+      enable = true;
       enableBashIntegration = true;
       enableZshIntegration = true;
     };
   };
 
-  home.sessionPath = [
-    "$HOME/.arkade/bin/"
-  ];
+  home.sessionPath = [ "$HOME/.arkade/bin/" ];
 
-  home.sessionVariables = {
-    EDITOR = "vim";
-  };
+  home.sessionVariables = { EDITOR = "vim"; };
+
+  fonts.fontconfig.enable = true;
 
   home.packages = [
     pkgs.arkade
     pkgs.devenv
     pkgs.btop
-    pkgs.github-copilot-cli
+    pkgs.nerd-fonts.fira-code
+    pkgs.nerd-fonts.iosevka
+  ] ++ lib.optionals pkgs.stdenv.isDarwin [
+    # Ensure at least bash v5 on macOS
+    pkgs.bash
   ];
 
-  nix = {
-    enable = true;
-    package = pkgs.nixVersions.nix_2_31;
-    settings = {
-      experimental-features = "nix-command flakes";
-      # builders = "ssh://nix@pi8.local aarch64-linux";
-      sandbox = true;
-      substituters = "https://cache.nixos.org https://welteki.cachix.org https://devenv.cachix.org";
-      trusted-public-keys = ''cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= welteki.cachix.org-1:zb0txiNEbjq9Fx7svp4LhTgFIQHKSa5ESi7QlLFjjQY= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw='';
-    };
-  };
+  # nix = {
+  #   enable = true;
+  #   settings = {
+  #     experimental-features = "nix-command flakes";
+  #     # builders = "ssh://nix@pi8.local aarch64-linux";
+  #     sandbox = true;
+  #     substituters =
+  #       "https://cache.nixos.org https://welteki.cachix.org https://devenv.cachix.org";
+  #     trusted-public-keys =
+  #       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= welteki.cachix.org-1:zb0txiNEbjq9Fx7svp4LhTgFIQHKSa5ESi7QlLFjjQY= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+  #   };
+  # };
 }
