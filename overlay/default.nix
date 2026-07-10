d6061315778205431dd9dca45f7fa0641e6900b2
Han Verstraete <welteki@pm.me>
2026-07-10 22:34:33 +0200

    herdr: init at v0.7.3

--- overlay/default.nix
+++ overlay/default.nix
@@ -8,6 +8,7 @@ in {
   actuated-cli = import ./actuated-cli final;
   caddy = import ./caddy final;
   hunk = import ./hunk inputs final;
+  herdr = inputs.herdr.packages.${final.stdenv.hostPlatform.system}.herdr;
   mass-deploy = import ./mass-deploy final;
   kubetrim = import ./kubetrim final;
 }
